dd126dfd5f59fefe8df41a9cdde995589e3b5519
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-14 23:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+async function loadStudents() {
+    try {
+        const res = await fetch("students.json");
+        const students = await res.json();
+
+        students.forEach(student => {
+            const marks = student.marks;
+            const avg = marks.reduce((a, b) => a + b, 0) / marks.length;
+            console.log(student.name + " " + avg);
+        });
+    } catch (e) {
+        console.log("Error reading file");
+    }
+}
+
+loadStudents();
+
