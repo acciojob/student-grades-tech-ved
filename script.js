d17ea1fc8e0d5cbdd4741a793d0019cfbf4e5516
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-03-01 23:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 import fs from "fs/promises";
 
-async function main() {
+async function solve() {
   try {
     const data = await fs.readFile("students.json", "utf-8");
     const students = JSON.parse(data);
 
-    students.forEach(student => {
-      const marks = student.marks;
+    students.forEach(s => {
       const avg =
-        marks.reduce((sum, m) => sum + m, 0) / marks.length;
+        s.marks.reduce((a, b) => a + b, 0) / s.marks.length;
 
-      console.log(`${student.name}: ${avg}`);
+      console.log(`${s.name}: ${avg}`);
     });
   } catch (err) {
     console.error(err);
   }
 }
 
-main();
+solve();
